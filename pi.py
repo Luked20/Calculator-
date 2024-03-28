0b1fc3ef0e34684351061c36da209491db23f0f0
Lucas <89111514+Luked20@users.noreply.github.com>
2024-03-28 18:15:56 -0300

    Update pi.py

--- pi.py
+++ pi.py
@@ -1,7 +1,10 @@
 from tkinter import *
 from tkinter import ttk
-
+import math
+from math import sqrt, sin 
 from colorama import Fore, Back, Style
+
+
  
 #inicialição da calculadora 
 init = Tk()
@@ -85,12 +88,14 @@ def open_second_window():
     second_window.geometry('357x420+0+0')
     text_parameter1 =Entry(second_window, width=20, font=("Arial", 24))  
     text_parameter1.grid(columnspan=5) 
-
+#Repetição da calculadora simples 
     def add_to_calc(symbol):
      global calcular
      calcular += str(symbol)
      text_parameter1.delete(0, "end")
      text_parameter1.insert(0 , calcular)
+     
+
 
     def evaluate_calc():
      global calcular
@@ -109,15 +114,54 @@ def open_second_window():
      text_parameter1.delete(0, "end")
      if(text_parameter == 0):
             return btn_0
-     
-  
-#problemas com a função calc_potencia o operador *= não satisfaz strings diferentemente de += tentando achar uma lógica para conseguir fazer a potenciação 
-    def calc_potencia(symbol):
-      global calcular
-      calcular *= str(symbol)
-      text_parameter.delete(0, "end")
-      text_parameter.insert(0, str(calcular))
-      
+#função para calcular a potencia
+    def pot(symbol):
+     global calcular
+     calcular += str(symbol)
+     modulos = ['math.pow']
+     for i in modulos:
+        if i in calcular:
+            calcular = calcular.replace("pow", "math.pow")
+    
+        resultado = eval(calcular)
+        text_parameter1.delete(0, "end")
+        text_parameter1.insert(0, str(resultado))
+    
+#função para calcular a raiz quadrada
+    def root(symbol):
+     global calcular
+     calcular += str(symbol)
+     modulos = ["√"]  
+     for i in modulos:
+        if i in calcular:
+            
+            calcular = calcular.replace("√", "")
+            try:
+                resultado = math.sqrt(eval(calcular))
+                text_parameter1.delete(0, "end")
+                text_parameter1.insert(0, str(resultado))
+            except Exception as e:
+                text_parameter1.delete(0, "end")
+                text_parameter1.insert(0, "Erro: " + str(e))
+    
+#atualmente tentando descobrir como fazer a função do seno funcionar para que usar o eval não esta dando certo 
+    def sin(symbol):
+     global calcular
+     calcular += str(symbol)
+     modulos = ["sin"]
+     for i in modulos:
+        if i in calcular:
+            calcular = calcular.replace("sin", "")
+            try:
+                resultado = math.sin(math.radians(eval(calcular)))  
+                text_parameter1.delete(0, "end")
+                text_parameter1.insert(0, str(resultado))
+            except Exception as e:
+                text_parameter1.delete(0, "end")
+                text_parameter1.insert(0, "Erro: " + str(e))
+         
+            
+   
     
         
        
@@ -145,8 +189,12 @@ def open_second_window():
     btn_9.grid(row=3, column=3)
     btn_0 = Button(second_window, text="0",command=lambda: add_to_calc(0), width=5, font=("Arial", 14) )
     btn_0.grid(row=4, column=2)
-    btn_pot = Button(second_window, text="^",command=lambda: calc_potencia("^"), width=5, font=("Arial", 14) )
-    btn_pot.grid(row=1, column=4)  
+    btn_pot = Button(second_window, text="^",command=lambda: pot("**"), width=5, font=("Arial", 14) )
+    btn_pot.grid(row=1, column=4) 
+    btn_root = Button(second_window, text="\u221a", command=lambda: root("√"), width=5, font=("Arial", 14))
+    btn_root.grid(row=2, column=4)
+    btn_sin = Button(second_window, text="sin", command=lambda: sin("sin"), width=5, font=("Arial", 14))
+    btn_sin.grid(row=3, column=4)
     btn_equal = Button(second_window, text="=",command= evaluate_calc, width=15, font=("Arial", 14) )
     btn_equal.grid(row=5, column=1, columnspan=2) 
     btn_clear = Button(second_window, text="C",command= clear_calc, width=5, font=("Arial", 14) )
